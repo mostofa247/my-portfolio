3d15f109c532b925b309f5aa9709eaf1c01e8a9c
unknown <mdshourov5596@gmail.com>
2023-09-01 18:51:03 +0600

    add live link

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAIIyoKplcmO9QpXOBs-HcfwdNbWSh3poA",
-  authDomain: "mostofa-sakib.firebaseapp.com",
-  projectId: "mostofa-sakib",
-  storageBucket: "mostofa-sakib.appspot.com",
-  messagingSenderId: "12926880336",
-  appId: "1:12926880336:web:adb8513e32664c20ac1f55",
+  apiKey: "AIzaSyAuNTkrsloT5Lc3Tq9uLXpdn6vpgBpuvsM",
+  authDomain: "mostofa-sw.firebaseapp.com",
+  projectId: "mostofa-sw",
+  storageBucket: "mostofa-sw.appspot.com",
+  messagingSenderId: "855111030412",
+  appId: "1:855111030412:web:4bc0c134fb10dc6f514da9",
 };
 
 // Initialize Firebase
